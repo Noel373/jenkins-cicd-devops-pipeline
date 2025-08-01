e729fcebaa0df24a4342c7c085297f192c248826
noeldevops <100243721+noeldevops@users.noreply.github.com>
2025-08-01 19:41:02 +0200

    Update worker-node-userdata.sh

--- kubernetes/kubeadmm cluster/worker-node-userdata.sh
+++ kubernetes/kubeadmm cluster/worker-node-userdata.sh
@@ -61,6 +61,8 @@ tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubelet kubeadm
 apt-mark hold kubelet kubeadm
+systemctl enable kubelet
+systemctl start kubelet
 
 echo "[7] Joining the Kubernetes cluster..."
 # NOTE: Replace below with your actual join command
